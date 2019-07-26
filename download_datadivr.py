e18a08f634b96b81714ff15d8b161361aef599b3
Eugenia Iofinova <eiofinova@cemm.oeaw.ac.at>
2019-07-26 11:52:03 +0200

    update download script

--- download_datadivr.py
+++ download_datadivr.py
@@ -4,8 +4,9 @@ import shutil
 import os
 import sys
 
-username_password_path = os.path.join(os.getcwd(), "asimov_auth")
-DATADIVR_PATH = os.path.realpath(os.path.join(os.path.dirname(os.getcwd()), "test.txt"))
+username_password_path = os.path.join(os.getcwd(), "ballmer.auth")
+DATADIVR_ZIP_PATH = os.path.realpath(os.path.join(os.path.dirname(os.getcwd()), "datadivr.zip"))
+DATADIVR_PATH = os.path.realpath(os.path.join(os.path.dirname(os.getcwd())))
 
 try:
     with open(username_password_path, 'r') as file:
@@ -15,22 +16,20 @@ try:
         password = lines[1][:-1]
         print(password)
 except Exception as e:
-    print("error occurred!")
+    print("An error occurred! Please make sure authentication file exists and is in this directory.")
     print(e)
     sys.exit()
 
 
-url = 'http://asimov.westeurope.cloudapp.azure.com:8080/jen_test/myfile.txt'
-filename = os.path.basename("test.txt")
+url = 'http://asimov.westeurope.cloudapp.azure.com:8080/datadivr/DataDiVR.zip'
+print("Downloading from ", url)
 
 r = requests.get(url, auth=(username,password))
+print("Download done!")
 
-with open(DATADIVR_PATH, 'wb') as file:
+with open(DATADIVR_ZIP_PATH, 'wb') as file:
     for chunk in r.iter_content():
         file.write(chunk)
-#response = requests.get(url)
 
-#urllib.request.urlretrieve(url, filename)
-
-#with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-#    zip_ref.extractall(directory_to_extract_to)
+with zipfile.ZipFile(DATADIVR_ZIP_PATH, 'r') as zip_ref:
+    zip_ref.extractall(DATADIVR_PATH)
